f3ce3cbdec2fbb9a9911f67576e131fa99d6b9b3
Ivan Toropygin <itori86@mail.ru>
2025-01-04 21:03:45 +0300

    add presentation

---
+++ app/src/main/java/com/sumin/numbercomposition/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,28 @@
+package com.sumin.numbercomposition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sumin.numbercomposition.databinding.FragmentChooseLevelBinding
+
+class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/sumin/numbercomposition/presentation/MainActivity.kt
+++ app/src/main/java/com/sumin/numbercomposition/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.sumin.numbercomposition.presentation
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.sumin.numbercomposition.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
